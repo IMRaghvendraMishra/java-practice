779cf23c8e8bb0a19eff949e7663e6c9af688c6b
rmishra <raghvendra.k.mishra@outlook.com>
2025-01-09 08:19:03 +0530

    Reformat file

--- src/main/java/org/practice/jdk8/streams/ProcessFile.java
+++ src/main/java/org/practice/jdk8/streams/ProcessFile.java
@@ -7,25 +7,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
-class Cat{
-    private String name, colour;
-    Cat(String name, String colour) {
-        this.name = name;
-        this.colour = colour;
-    }
-    @Override
-    public String toString() {
-        return "Cat{" + "name=" + name + ", colour=" + colour + '}';
-    }
-}
 public class ProcessFile {
-    public static void main(String []args){
+    public static void main(String[] args) {
         List<Cat> cats = loadCats("Cats.txt");
         cats.forEach(System.out::println);// just print the Cat
     }
-    public static List<Cat> loadCats(String filename){
+
+    public static List<Cat> loadCats(String filename) {
         List<Cat> cats = new ArrayList<>();
-        try(Stream<String> stream = Files.lines(Paths.get(filename))){
+        try (Stream<String> stream = Files.lines(Paths.get(filename))) {
             stream.forEach(line -> {
                 String[] catsArray = line.split("/");
                 cats.add(new Cat(catsArray[0], catsArray[1]));
@@ -34,6 +24,20 @@ public class ProcessFile {
             ioe.printStackTrace();
         }
         return cats;
+    }
+
+    static class Cat {
+        private String name, colour;
+
+        Cat(String name, String colour) {
+            this.name = name;
+            this.colour = colour;
+        }
+
+        @Override
+        public String toString() {
+            return "Cat{" + "name=" + name + ", colour=" + colour + '}';
+        }
     }
 }
 
